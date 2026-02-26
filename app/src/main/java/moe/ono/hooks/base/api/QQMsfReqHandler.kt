43bd8437ec948857e1521aa2c09e0c3f6fc496a8
cwuom <cwuomcwuom@163.com>
2026-02-27 00:50:18 +0800

    refactor: refine HookItemLoader process matching and clean up code
    - Fix process matching logic in `HookItemLoader` by using bitwise AND to support multiple target processes.
    - Update `FakeGroupOwner` description.
    - Remove redundant logging in `QQMsfReqHandler`.
    - Add `Debugger.kt` to `.gitignore`.

--- app/src/main/java/moe/ono/hooks/base/api/QQMsfReqHandler.kt
+++ app/src/main/java/moe/ono/hooks/base/api/QQMsfReqHandler.kt
@@ -8,7 +8,6 @@ import moe.ono.hooks._base.ApiHookItem
 import moe.ono.hooks._core.annotation.HookItem
 import moe.ono.loader.hookapi.IMsfHandler
 import moe.ono.util.Initiator.loadClass
-import moe.ono.util.Logger
 
 
 @HookItem(path = "API/QQ MSF 数据包监听")
@@ -25,7 +24,7 @@ class QQMsfReqHandler : ApiHookItem() {
                 override fun beforeHookedMethod(param: MethodHookParam) {
                     val toServiceMsg = param.args[1] as ToServiceMsg
                     val intent = param.args[0] as Intent
-                    Logger.i(toServiceMsg.serviceCmd)
+
                     handler.forEach { handler ->
                         if (toServiceMsg.serviceCmd == handler.cmd) {
                             handler.onHandle(param, intent, toServiceMsg)
